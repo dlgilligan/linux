3c8521e759df79119891c1ec999d9ef04c5a1a36
dlgilligan <danny.gilligan26@gmail.com>
2026-04-11 23:10:04 -0400

    Man focus

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,11 +1,20 @@
 -- Auto Commands
 
+-- When running ':Man' command, focus on window
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "man",
+	callback = function()
+		vim.cmd("only")
+	end,
+})
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function() vim.hl.on_yank() end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
-
